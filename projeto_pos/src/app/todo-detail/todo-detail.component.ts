6f4792101e5399a900b6cfa596897b065b515ed7
arthur-mendonca <arthu91@gmail.com>
2024-07-26 15:06:05 -0300

    feat: criar lógica para mostrar cada todo individualmente

--- projeto_pos/src/app/todo-detail/todo-detail.component.ts
+++ projeto_pos/src/app/todo-detail/todo-detail.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { TodoService } from "../todo.service";
+import { Todo } from "../todo.model";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-todo-detail",
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: "./todo-detail.component.html",
   styleUrl: "./todo-detail.component.css",
 })
 export class TodoDetailComponent implements OnInit {
-  todo: any;
+  todo: Todo = new Todo();
 
   constructor(
     private route: ActivatedRoute,
-    private todoService: TodoService
+    private todoService: TodoService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -23,5 +27,36 @@ export class TodoDetailComponent implements OnInit {
     this.todoService.getTodoById(id).subscribe((data) => {
       this.todo = data;
     });
+  }
+
+  updateTodo(): void {
+    if (this.todo.id) {
+      this.todoService
+        .updateTodoById(this.todo.id.toString(), this.todo)
+        .subscribe(
+          (response) => {
+            console.log("To-do updated successfully");
+            this.router.navigate(["/"]);
+          },
+
+          (error) => {
+            console.log("Error updating To-do", error);
+          }
+        );
+    }
+  }
+
+  deleteTodo(): void {
+    if (this.todo.id) {
+      this.todoService.deleteTodoById(this.todo.id.toString()).subscribe(
+        (response) => {
+          console.log("To-do deleted successfully");
+          this.router.navigate(["/"]);
+        },
+        (error) => {
+          console.log("Error deleting To-do", error);
+        }
+      );
+    }
   }
 }
