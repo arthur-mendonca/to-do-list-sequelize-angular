99031f6713b697124a6540c5d656141a7e96f124
arthur-mendonca <arthu91@gmail.com>
2024-07-26 10:55:12 -0300

    fix: remover app.component padrão

--- projeto_pos/src/app/app.component.ts
+++ projeto_pos/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { TodoListComponent } from "./todo-list/todo-list.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, TodoListComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'projeto_pos';
+  title = "projeto_pos";
 }
