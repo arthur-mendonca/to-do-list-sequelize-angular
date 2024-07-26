0c2c243addebd7030c316f8458f2de22999b03d0
arthur-mendonca <arthu91@gmail.com>
2024-07-26 15:05:07 -0300

    fix: consertar rota de criar um novo todo

--- projeto_pos/src/app/app.routes.ts
+++ projeto_pos/src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { TodoListComponent } from "./todo-list/todo-list.component";
+
+import { TodoUpdateComponent } from "./todo-update/todo-update.component";
 import { TodoDetailComponent } from "./todo-detail/todo-detail.component";
 import { TodoCreateComponent } from "./todo-create/todo-create.component";
-import { TodoUpdateComponent } from "./todo-update/todo-update.component";
 
 export const routes: Routes = [
-  { path: "", component: TodoListComponent },
-  {
-    path: "todos/:id",
-    component: TodoDetailComponent,
-  },
   {
     path: "todos/create",
     component: TodoCreateComponent,
@@ -18,5 +14,10 @@ export const routes: Routes = [
   {
     path: "todos/update/:id",
     component: TodoUpdateComponent,
+  },
+  { path: "", component: TodoListComponent },
+  {
+    path: "todos/:id",
+    component: TodoDetailComponent,
   },
 ];
